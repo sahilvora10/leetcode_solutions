6f0fa7d1ea263bd1bcba6ed65efbb2baacd0df9a
SAHIL VORA <30970675+sahilvora10@users.noreply.github.com>
2022-08-21 23:29:14 +0000

    Sync LeetCode submission - Roman to Integer (python3)

---
+++ my-folder/problems/roman_to_integer/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        rdict = {
+            'I' : 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+            'IV': 4,
+            'IX': 9,
+            'XL': 40,
+            'XC': 90,
+            'CD':400,
+            'CM':900
+        }
+        val = 0
+        # print(s[0:1])
+        i = 0
+        while i < len(s):
+            if s[i:i+2] in rdict:
+                val = val + rdict[s[i:i+2]]
+                i += 2
+            else:
+                val = val+rdict[s[i]]
+                i += 1
+        return val
