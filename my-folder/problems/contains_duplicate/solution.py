2def944499e16e0561d7012d42bba0142dcd0e5a
SAHIL VORA <30970675+sahilvora10@users.noreply.github.com>
2022-09-16 08:12:07 +0000

    Sync LeetCode submission - Contains Duplicate (python3)

---
+++ my-folder/problems/contains_duplicate/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dic = set()
+        for n in nums:
+            if n in dic:
+                return True
+            dic.add(n)
+        return False 
+        
