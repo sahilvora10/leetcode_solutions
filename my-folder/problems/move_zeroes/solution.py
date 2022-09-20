8e3a8f3c509148860e675b634fff013599b619b6
SAHIL VORA <30970675+sahilvora10@users.noreply.github.com>
2022-09-20 06:58:31 +0000

    Sync LeetCode submission - Move Zeroes (python3)

--- my-folder/problems/move_zeroes/solution.py
+++ my-folder/problems/move_zeroes/solution.py
@@ -7,27 +7,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # n = len(nums)
-        # if n != 1:
-        #     i = 0
-        #     for i in range(n):
-        #         if nums[i] == 0:
-        #             break
-        #         else:
-        #             i +=1
-        #     s = i
-        #     j = i+1
-        #     while s<n and j<n:
-        #         if nums[s] == 0 and nums[j]!=0:
-        #             swap(nums,s,j)
-        #             i+=1
-        #             j+=1
-        #         elif nums[j] == 0:
-        #             j+=1
-        #         else:
-        #             continue
         i = 0
         for j in range(len(nums)):
             if nums[j] != 0:
                 nums[i], nums[j] = nums[j], nums[i]
+                # print(nums)
                 i += 1
