95a2155a76654bd8b845b1f5cb77223ecb598a58
SAHIL VORA <30970675+sahilvora10@users.noreply.github.com>
2022-09-24 10:13:17 +0000

    Sync LeetCode submission - Single Number II (python3)

--- my-folder/problems/single_number_ii/solution.py
+++ my-folder/problems/single_number_ii/solution.py
@@ -1,22 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # using an extra memory
-        hashkey = {}
-        for n in nums:
-            if n in hashkey:
-                hashkey[n] += 1
-            else:
-                hashkey[n] = 1
-        for k,v in hashkey.items():
-            if v == 1:
-                return k
-        return -1
-        
-        # c = Counter(nums)
-        # for k,v in c.items():
-        #     if v == 1:
-        #         return k
-        
         #without extra memory
+        ones = 0
+        twos = 0
+        for n in nums:
+            ones = (ones^n)&(~twos)
+            twos = (twos^n)&(~ones)
+            print(ones,twos)
+        return ones
         
     
