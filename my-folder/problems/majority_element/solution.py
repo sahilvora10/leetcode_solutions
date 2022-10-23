c6023b055b47ae590bab7aed617e178b8343caaf
SAHIL VORA <30970675+sahilvora10@users.noreply.github.com>
2022-10-23 22:15:05 +0000

    Sync LeetCode submission - Majority Element (python3)

--- my-folder/problems/majority_element/solution.py
+++ my-folder/problems/majority_element/solution.py
@@ -1,12 +1,23 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hm = {}
-        for n in nums:
-            if n in hm:
-                hm[n]+=1
-                if hm[n]>(len(nums)//2):
-                    return n
+        #brute force
+        # hm = {}
+        # for n in nums:
+        #     if n in hm:
+        #         hm[n]+=1
+        #         if hm[n]>(len(nums)//2):
+        #             return n
+        #     else:
+        #         hm[n] = 1
+        # return nums[0]
+        #moore voting algo
+        count = 0
+        element = 0
+        for i in range(len(nums)):
+            if count == 0:
+                element=nums[i]
+            if element == nums[i]:
+                count+=1
             else:
-                hm[n] = 1
-        return nums[0]
-        
+                count-=1
+        return element
