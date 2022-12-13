d1609023b9018f7c6f22596709547cc372cf3235
SAHIL VORA <30970675+sahilvora10@users.noreply.github.com>
2022-12-13 03:38:04 +0000

    Sync LeetCode submission - Find Peak Element (python3)

--- my-folder/problems/find_peak_element/solution.py
+++ my-folder/problems/find_peak_element/solution.py
@@ -4,7 +4,7 @@ class Solution:
     
     def binary(self,nums,l,r):
         while l<=r:
-            mid = l + (r-l)//2
+            mid =(l+r)//2
             if (mid == 0 or nums[mid]>=nums[mid-1]) and (mid == len(nums)-1 or nums[mid]>=nums[mid+1]):
                 return mid
             elif mid>0 and nums[mid-1]>nums[mid]:
