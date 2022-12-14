5ebca5854c486976e3d92d13de086ca46df58577
SAHIL VORA <30970675+sahilvora10@users.noreply.github.com>
2022-12-14 18:46:02 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (python3)

--- my-folder/problems/binary_tree_inorder_traversal/solution.py
+++ my-folder/problems/binary_tree_inorder_traversal/solution.py
@@ -6,12 +6,54 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        #recursive
+        # l = []
+        # def inorder(root):
+        #     if root:
+        #         inorder(root.left)
+        #         l.append(root.val)
+        #         inorder(root.right)
+        # inorder(root)
+        # return l
+        
+        #iterative
+        stack = deque()
         l = []
-        def inorder(root):
-            if root:
-                inorder(root.left)
-                l.append(root.val)
-                inorder(root.right)
-        inorder(root)
+        node = root
+        while True:
+            if node!=None:
+                stack.append(node)
+                node = node.left
+            else:
+                if not stack:
+                    break
+                node = stack.pop()
+                l.append(node.val)
+                node = node.right
         return l
+        
+        # preorder = []
+        # inorder = []
+        # postorder = []
+        # stack = deque()
+        # stack.append((root,1))
+        # while stack:
+        #     node,num = stack.pop()
+        #     if node:
+        #         if num==1:
+        #             preorder.append(node.val)
+        #             stack.append((node,num+1))
+        #             if node.left:
+        #                 stack.append((node.left,1))
+        #         elif num==2:
+        #             inorder.append(node.val)
+        #             stack.append((node,num+1))
+        #             if node.right:
+        #                 stack.append((node.right,1))
+        #         else:
+        #             postorder.append(node.val)
+        # print(preorder)
+        # print(inorder)
+        # print(postorder)
+        # return inorder
         
